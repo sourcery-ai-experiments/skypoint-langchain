13cdeef10b845f495f48d589699b53da5e949806
sushant <sushant.burnawal@skypointcloud.com>
2024-04-01 19:14:11 +0530

    add changes to sqlcoder prompt

--- libs/langchain/langchain/tools/sqlcoder/prompt.py
+++ libs/langchain/langchain/tools/sqlcoder/prompt.py
@@ -7,7 +7,7 @@ Adhere to these rules:
 You are an AI research assistant in the senior living industry.
 You have access to a database that contains the information about different communities, their amenities, residents, expenses, budget, revenue and other finances, facilities, beds, events
 When querying the database, given an input question, create a syntactically correct query to run, then look at the results of the query and return the answer.
-Unless the user specifies a specific number of examples they wish to obtain, always limit your query to at most 10 results.
+Unless the user specifies a specific number of examples they wish to obtain, always limit your query to at most 30 results. 
 You can order the results by a relevant column to return the most interesting examples in the database.
 Never query for all the columns from a specific table, only ask for the relevant columns given the question.
 DO NOT make any DML statements (INSERT, UPDATE, DELETE, DROP etc.) to the database.
@@ -18,6 +18,7 @@ This query will run on a database whose schema is represented in this string:
 '{db_schema}'
 Use the following examples to generate the sql query:
 '{few_shot_examples}'
+Unless specified in the user input, always limit your query to 30 results
 ### Response:
 Based on your instructions, here is the SQL query I have generated to answer '{user_input}'
 ```sql"""
@@ -25,6 +26,5 @@ Based on your instructions, here is the SQL query I have generated to answer '{u
 
 SQL_QUERY_CREATOR_RETRY  = """ 
 You have failed in the first attempt to generate correct sql query. Please try again to generate correct sql query.
-The previously generated queries are {sql_query}.
-Make sure you create right query by using the schema of the table and few_shot_examples.
+Make sure you create right query by using the {db_schema}, {few_shot_examples} and do not repeat any query from the previously generated queries of {sql_query}.
 """
